cbe0505c065453094c7ba2a52e602e28f2bcf2c5
xiazhiqiang <xiazhiqiangcuc@126.com>
2021-08-15 23:15:54 +0800

    斐波那契数列

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,3 +7,45 @@ export const defaultToString = (p: any) => {
     return p.toString();
   }
 };
+
+export const factorialIterative = (n: number): number => {
+  if (n > 1) {
+    return n * factorialIterative(n - 1);
+  } else {
+    return 1;
+  }
+};
+
+export const fibonacciIterative = (n: number): number => {
+  if (n < 1) {
+    return 0;
+  }
+  if (n <= 2) {
+    return 1;
+  }
+  let f1 = 0;
+  let f2 = 1;
+  let fi: number = 1;
+  for (let i = 2; i <= n; i++) {
+    fi = f1 + f2;
+    f1 = f2;
+    f2 = fi;
+  }
+
+  return fi;
+};
+
+/**
+ * 斐波那契数列
+ */
+export const fibonacciMemoization = () => {
+  let memo = [0, 1];
+  const fibonacci = (n: number): number => {
+    if (memo[n] !== undefined) {
+      return memo[n];
+    }
+    memo[n] = fibonacci(n - 1) + fibonacci(n - 2);
+    return memo[n];
+  };
+  return fibonacci;
+};
